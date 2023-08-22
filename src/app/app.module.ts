2db5cbc65c381182d8c210964994475d5bb9de35
RoeiLevy <roeilevy99@gmail.com>
2023-08-23 00:37:15 +0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { ModelDrivenComponent } from './componentes/model-driven/model-driven.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ModelDrivenComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    ModelDrivenComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/componentes/model-driven/model-driven.component.ts
+++ src/app/componentes/model-driven/model-driven.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { Customer } from 'src/app/interfaces/customer';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'model-driven',
   templateUrl: './model-driven.component.html',
   styleUrls: ['./model-driven.component.scss'],
 })
+
 export class ModelDrivenComponent {
   customerForm: FormGroup;
   languages = ['English', 'Spanish', 'French'];
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  matcher = new MyErrorStateMatcher();
+  @ViewChild('fileInputRef', { static: false }) fileInputRef!: ElementRef<HTMLInputElement>;
+  isLoadingImage: Boolean = false;
+  fileInputError: string | null = null;
 
   constructor(private formBuilder: FormBuilder) {
     this.customerForm = this.formBuilder.group({
-      fullName: ['', Validators.required],
-      preferredLanguage: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
-      faceImage: [null, Validators.required],
+      fullName: ['', [Validators.required, Validators.pattern(/^[A-Za-z\s]+$/)]],
+      preferredLanguage: ['', [Validators.required]],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      faceImage: [null, [Validators.required]]
     });
   }
 
@@ -27,14 +42,33 @@ export class ModelDrivenComponent {
   }
 
   onImageChange(event: Event) {
+    this.isLoadingImage = true;
+    const startLoadingTime = Date.now();
     const inputElement = event.target as HTMLInputElement;
     const file = inputElement.files?.[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = (e) => {
         this.customerForm.patchValue({ faceImage: e.target?.result || '' });
+        console.log(this.customerForm.value.faceImage);
+
+        const finishLoadingTime = Date.now();
+        if (finishLoadingTime - startLoadingTime < 3000) {
+          setTimeout(() => {
+            this.isLoadingImage = false;
+          }, 3000 - (finishLoadingTime - startLoadingTime))
+
+        } else this.isLoadingImage = false;
       };
       reader.readAsDataURL(file);
     }
+  }
+
+  nativeInputClick() {
+    this.fileInputRef.nativeElement.click();
+  }
+
+  onFileError() {
+    this.fileInputError = 'An error occurred with uploading the image. Please try again.';
   }
 }
